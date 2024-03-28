ef49bf7a760e86fe66ac2e3ba4e057905b6debfd
Felix Perfler <me@felixperfler.com>
2024-03-28 14:22:06 +0100

    added basic files

---
+++ hyba/hybridfilterbank.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class HybridFilterbank(nn.Module):
+
+    def __init__(self):
+         super().__init__()
+        
+        pass
+        
+    def forward(self, x):
+    
+        pass
