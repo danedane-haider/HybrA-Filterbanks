96331bedff91c176dc7dc76e51fc24b5a50bf83e
Daniel Haider <55834940+danedane-haider@users.noreply.github.com>
2024-06-24 15:27:15 +0200

    smooth

--- hybra/utils.py
+++ hybra/utils.py
@@ -15,10 +15,22 @@ def calculate_condition_number(w) -> torch.Tensor:
 
 def audfilters(n_filters, filter_length, hop_length, frequency_scale, sr):
     """""
-    constructs a set of filters *g* that are equidistantly spaced on a perceptual frequency scale (see |freqtoaud|) between 0 and the Nyquist frequency.
+    Constructs a set of filters *g* that are equidistantly spaced on a perceptual frequency scale (see |freqtoaud|) between 0 and the Nyquist frequency.
     The filter bandwidths are proportional to the  critical bandwidth of the auditory filters |audfiltbw|.
     The filters are intended to work with signals with a sampling rate of *fs*.
     The signal length *Ls* is mandatory, since we need to avoid too narrow frequency windows.
     """""
     g = np.zeros((n_filters, filter_length), dtype=np.complex64)
     return g
+
+def smooth_fir(frequency_responses, support):
+    """""
+    Takes a matrix of frequency responses (as columns) and constructs a smoothed FIR version with support length *support*.
+    """""
+    g = np.exp(-np.pi * np.arange(-support//2,support//2)**2 / ((support-12)/2)**2)
+    supper = g.reshape(1,-1)
+    gi = np.fft.ifft(frequency_responses, axis=0)
+    gi = np.roll(gi, support//2, axis=0)
+    g_re = np.real(gi[:support]).T * supper
+    g_im = np.imag(gi[:support]).T * supper
+    return g_re + 1j * g_im
