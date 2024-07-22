e57a50a9432490abf84bfc6417f4e520d2da2b27
Felix Perfler <me@felixperfler.com>
2024-07-22 23:47:39 +0200

    now functional

--- hybra/hybridfilterbank.py
+++ hybra/hybridfilterbank.py
@@ -2,31 +2,35 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from hybra.utils import audfilters
-import os
-
-_current_dir: str = os.path.dirname(os.path.realpath(__file__))
+from hybra.utils import calculate_condition_number, fir_tightener3000
 
 class HybrA(nn.Module):
-
-    def __init__(self, n_filters:int=256, filter_length:int=512, hop_length:int=128, f_scale:str='erb', sr:int=16000, kernel_size:int=11, pretrained:bool=True):
+    def __init__(self, path_to_auditory_filter_config, start_tight:bool=True):
         super().__init__()
 
-        if pretrained:
-            with open(_current_dir+"/ressources/auditory_filters_example.npy", "rb") as f:
-                self.auditory_filters = torch.tensor(np.load(f), dtype=torch.complex64)
-        else:
-            self.auditory_filters = audfilters(n_filters, filter_length, hop_length, f_scale, sr)
+        config = torch.load(path_to_auditory_filter_config)
         
-        self.auditory_filters_real = torch.flip(self.auditory_filters.real, dims=(1,))
-        self.auditory_filters_imag = torch.flip(self.auditory_filters.imag, dims=(1,))
+        self.auditory_filters_real = torch.tensor(config['auditory_filters_real'])
+        self.auditory_filters_imag = torch.tensor(config['auditory_filters_imag'])
+        self.auditory_filters_stride = config['auditory_filters_stride']
+        n_filters = config['n_filters']
+        kernel_size = config['kernel_size']
 
-        self.conv1d_encoder = nn.Conv1d(in_channels=n_filters, out_channels=n_filters, kernel_size=kernel_size, stride=1, padding='same', bias=False, groups=n_filters)
-        self.conv1d_decoder = nn.Conv1d(in_channels=n_filters, out_channels=n_filters, kernel_size=kernel_size, stride=1, padding='same', bias=False, groups=n_filters)
+        self.output_real_forward = None
+        self.output_imag_forward = None
 
-        self.n_filters = n_filters
-        self.filter_length = filter_length
-        self.hop_length = hop_length
+        self.encoder = nn.Conv1d(
+             in_channels=n_filters,
+             out_channels=n_filters,
+             kernel_size=kernel_size,
+             stride=1, padding='same',
+             bias=False,
+             groups=n_filters)
+    
+        if start_tight:
+            self.encoder.weight = nn.Parameter(torch.tensor(fir_tightener3000(
+                self.encoder.weight.squeeze(1).detach().numpy(), kernel_size, eps=1.01
+            ),  dtype=torch.float32).unsqueeze(1))
         
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         """Forward pass of the HybridFilterbank.
@@ -39,11 +43,25 @@ class HybrA(nn.Module):
         --------
         x (torch.Tensor) - output tensor of shape (batch_size, n_filters, signal_length//hop_length)
         """
-        hybrid_encoder_real = self.conv1d_encoder(self.auditory_filters_real).unsqueeze(1)
-        hybrid_encoder_imag = self.conv1d_encoder(self.auditory_filters_imag).unsqueeze(1)
-        x_real = F.conv1d(x, hybrid_encoder_real, stride=self.hop_length, padding=0)
-        x_imag = F.conv1d(x, hybrid_encoder_imag, stride=self.hop_length, padding=0)
-        return x_real + 1j * x_imag
+        output_real = F.conv1d(
+            x.unsqueeze(1),
+            self.auditory_filters_real.to(x.device),
+            stride=self.auditory_filters_stride,
+            padding=0,
+        )
+        self.output_real_forward = output_real.clone().detach()
+        output_imag = F.conv1d(
+            x.unsqueeze(1),
+            self.auditory_filters_imag.to(x.device),
+            stride=self.auditory_filters_stride,
+            padding=0,
+        )
+        self.output_imag_forward = output_imag.clone().detach()
+
+        output_real = self.encoder(output_real)
+        output_imag = self.encoder(output_imag)
+
+        return output_real**2 + output_imag**2
     
     def decoder(self, x:torch.Tensor) -> torch.Tensor:
         """Forward pass of the dual HybridFilterbank.
@@ -54,10 +72,28 @@ class HybrA(nn.Module):
 
         Returns:
         --------
-        x (torch.Tensor) - output tensor of shape (batch_size, 1, signal_length)
+        x (torch.Tensor) - output tensor of shape (batch_size, signal_length)
         """
-        hybrid_decoder_real = self.conv1d_decoder(self.auditory_filters_real).unsqueeze(1)
-        hybrid_decoder_imag = self.conv1d_decoder(self.auditory_filters_imag).unsqueeze(1)
-        x_real = F.conv_transpose1d(x.real, hybrid_decoder_real, stride=self.hop_length, padding=0)
-        x_imag = F.conv_transpose1d(x.imag, hybrid_decoder_imag, stride=self.hop_length, padding=0)
-        return self.hop_length * (x_real + 1j * x_imag)
+        x_real = x * self.output_real_forward
+        x_imag = x * self.output_imag_forward
+        x = (
+            F.conv_transpose1d(
+                x_real,
+                self.auditory_filters_real.to(x_real.device),
+                stride=self.auditory_filters_stride,
+                padding=0,
+            )
+            + F.conv_transpose1d(
+                x_imag,
+                self.auditory_filters_imag.to(x_imag.device),
+                stride=self.auditory_filters_stride,
+                padding=0,
+            )
+        )
+
+        return x.squeeze(1)
+
+    @property
+    def condition_number(self):
+        coefficients = self.encoder.weight.detach().clone().squeeze(1)
+        return float(calculate_condition_number(coefficients))
