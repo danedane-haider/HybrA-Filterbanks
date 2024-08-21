2a6d4ab6dda4a0525c4530dab2db9840c1738240
Felix Perfler <me@felixperfler.com>
2024-08-21 16:38:46 +0200

    goooo

--- hybra/hybridfilterbank.py
+++ hybra/hybridfilterbank.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from hybra.utils import calculate_condition_number, fir_tightener3000
@@ -10,29 +9,31 @@ class HybrA(nn.Module):
 
         self.skip_connection = skip_connection
         config = torch.load(path_to_auditory_filter_config, weights_only=False, map_location="cpu")
-        
+
         self.auditory_filters_real = torch.tensor(config['auditory_filters_real'])
         self.auditory_filters_imag = torch.tensor(config['auditory_filters_imag'])
         self.auditory_filters_stride = config['auditory_filters_stride']
-        n_filters = config['n_filters']
-        kernel_size = config['kernel_size']
+        self.auditory_filter_length = self.auditory_filters_real.shape[-1]
+        self.n_filters = config['n_filters']
+        self.kernel_size = config['kernel_size']
 
         self.output_real_forward = None
         self.output_imag_forward = None
 
-        self.encoder = nn.Conv1d(
-             in_channels=n_filters,
-             out_channels=n_filters,
-             kernel_size=kernel_size,
-             stride=1, padding='same',
-             bias=False,
-             groups=n_filters)
-    
+        k = torch.tensor(self.n_filters / (self.kernel_size * self.n_filters))
+        encoder_weight = (-torch.sqrt(k) - torch.sqrt(k)) * torch.rand([self.n_filters, 1, self.kernel_size]) + torch.sqrt(k)
+
         if start_tight:
-            self.encoder.weight = nn.Parameter(torch.tensor(fir_tightener3000(
-                self.encoder.weight.squeeze(1).detach().numpy(), kernel_size, eps=1.01
-            ),  dtype=torch.float32).unsqueeze(1))
+            encoder_weight = torch.tensor(fir_tightener3000(
+                encoder_weight.squeeze(1).numpy(), self.kernel_size, eps=1.01
+            ),  dtype=torch.float32).unsqueeze(1)
         
+        self.encoder_weight_real = nn.Parameter(encoder_weight, requires_grad=True)
+        self.encoder_weight_imag = nn.Parameter(encoder_weight, requires_grad=True)
+
+        self.hybra_filters_real = torch.empty(1)
+        self.hybra_filters_imag = torch.empty(1)
+
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         """Forward pass of the HybridFilterbank.
         
@@ -44,29 +45,43 @@ class HybrA(nn.Module):
         --------
         x (torch.Tensor) - output tensor of shape (batch_size, n_filters, signal_length//hop_length)
         """
-        output_real = F.conv1d(
-            x.unsqueeze(1),
-            self.auditory_filters_real.to(x.device),
-            stride=self.auditory_filters_stride,
-            padding=0,
-        )
-        self.output_real_forward = output_real.clone().detach()
-        output_imag = F.conv1d(
-            x.unsqueeze(1),
-            self.auditory_filters_imag.to(x.device),
-            stride=self.auditory_filters_stride,
-            padding=0,
-        )
-        self.output_imag_forward = output_imag.clone().detach()
 
-        out = self.encoder(output_real)**2 + self.encoder(output_imag)**2
+        kernel_real = F.conv1d(
+            self.auditory_filters_real.to(x.device).squeeze(1),
+            self.encoder_weight_real,
+            groups=self.n_filters,
+            padding="same",
+        ).unsqueeze(1)
+        self.hybra_filters_real = kernel_real.clone().detach()
+
+        kernel_imag = F.conv1d(
+            self.auditory_filters_imag.to(x.device).squeeze(1),
+            self.encoder_weight_imag,
+            groups=self.n_filters,
+            padding="same",
+        ).unsqueeze(1)
+        self.hybra_filters_imag = kernel_imag.clone().detach()
+        
+        output_real = F.conv1d(
+            F.pad(x.unsqueeze(1), (self.auditory_filter_length//2, self.auditory_filter_length//2), mode='circular'),
+            kernel_real,
+            stride=self.auditory_filters_stride,
+        )
+        
+        output_imag = F.conv1d(
+            F.pad(x.unsqueeze(1), (self.auditory_filter_length//2,self.auditory_filter_length//2), mode='circular'),
+            kernel_imag,
+            stride=self.auditory_filters_stride,
+        )
+
+        out = output_real + 1j* output_imag
 
         if self.skip_connection:
-            out += output_real**2 + output_imag**2
+            out += output_real + output_imag
 
         return out
     
-    def decoder(self, x:torch.Tensor) -> torch.Tensor:
+    def decoder(self, x_real:torch.Tensor, x_imag:torch.Tensor) -> torch.Tensor:
         """Forward pass of the dual HybridFilterbank.
 
         Parameters:
@@ -77,25 +92,18 @@ class HybrA(nn.Module):
         --------
         x (torch.Tensor) - output tensor of shape (batch_size, signal_length)
         """
-        if self.skip_connection:
-            x_real = x * self.output_real_forward + x
-            x_imag = x * self.output_imag_forward + x
-        else:
-            x_real = x * self.output_real_forward
-            x_imag = x * self.output_imag_forward
-
         x = (
             F.conv_transpose1d(
                 x_real,
-                self.auditory_filters_real.to(x_real.device),
+                self.hybra_filters_real,
                 stride=self.auditory_filters_stride,
-                padding=0,
+                padding=self.auditory_filter_length//2,
             )
             + F.conv_transpose1d(
                 x_imag,
-                self.auditory_filters_imag.to(x_imag.device),
+                self.hybra_filters_imag,
                 stride=self.auditory_filters_stride,
-                padding=0,
+                padding=self.auditory_filter_length//2,
             )
         )
 
@@ -103,5 +111,5 @@ class HybrA(nn.Module):
 
     @property
     def condition_number(self):
-        coefficients = self.encoder.weight.detach().clone().squeeze(1)
+        coefficients = self.hybra_filters_real.detach().clone().squeeze(1) + 1j* self.hybra_filters_imag.detach().clone().squeeze(1)
         return float(calculate_condition_number(coefficients))
