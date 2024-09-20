3f9e6682e49907815676c1b5dae51c5815f3986e
Felix Perfler <me@felixperfler.com>
2024-09-20 15:47:19 +0200

    sorry

--- hybra/utils.py
+++ hybra/utils.py
@@ -1,27 +1,5 @@
 import numpy as np
 import torch
-import tqdm
-
-def random_filterbank(N, J, T, norm=True, support_only=False):
-    """""
-    Constructs a random filterbank of J filters of support T, padded with zeros to have length N.
-    Input: N (int) - signal length
-              J (int) - number of filters
-                T (int) - support of the filter or length of learned conv1d kernel (default: T=N)
-    Output: kappa (torch.tensor) - condition number of the convolution operator
-    """""
-    if T == None:
-        T = N
-    if norm:
-        w = torch.randn(J, T).div(torch.sqrt(torch.tensor(J*T)))
-    else:
-        w = torch.randn(J, T)
-    if support_only:
-        w_cat = w
-    else:
-        w_cat = torch.cat([w, torch.zeros(J, N-T)], dim=1)
-    return w_cat
-
 
 def random_filterbank(N, J, T, norm=True, support_only=False):
     """""
@@ -121,37 +99,41 @@ def can_tight(w, D=1):
         S_inv_sqrt = (U @ torch.diag(lam_square).to(torch.complex64) @ U.T).to(torch.float32)
         return (S_inv_sqrt @ w.T).T
 
-def frame_bounds_lp(w, freq=False):
-    # if the filters are given already as frequency responses
-    if freq:
-        w_hat = np.sum(np.abs(w) ** 2, axis=1)
-    else:
-        w_hat = np.sum(np.abs(np.fft.fft(w, axis=1)) ** 2, axis=0)
-    B = np.max(w_hat)
-    A = np.min(w_hat)
-
-    return A, B
-
-def can_tight(w):
+def fb_analysis(w, D):
     """
-    Construction of the canonical tight filterbank
+    Construction of the analysis operator matrix having all shifted copies of the filters as rows
     :param w: analysis filterbank
-    :return: canonical tight filterbank
+    :param D: decimation factor
+    :return: analysis operator matrix
     """
-    w_freqz = torch.fft.fft(w, dim=1)
-    lp = torch.sum(w_freqz.abs() ** 2, dim=0)
-    w_freqz_tight = w_freqz * lp ** (-0.5)
-    w_tight = torch.fft.ifft(w_freqz_tight, dim=1)
-    return w_tight
+    N = w.shape[1]
+    J = w.shape[0]
+    w = w.flip((1,)).roll(1, 1)
+    W = torch.cat([w.flip((1,)), torch.narrow(w.flip((1,)), dim=1, start=0, length=N-1)], dim=1)
+    W = W.unfold(1, N, 1).flip((-1,)).reshape(J*N, N)
+    return W[::D, :]
 
-def frame_bounds(w, frequency_domain=False):
-    if frequency_domain:
-        w_hat = torch.sum(w.abs() ** 2, dim=1)
+def kappa_alias(w, D):
+    """
+    Computes the condition number and aliasing term of the filterbank
+    :param w: analysis filterbank
+    :param D: decimation factor
+    :return: kappa, alias - condition number and aliasing term
+    """
+    w_hat = torch.fft.fft(w, dim=1)
+    diag = torch.sum(w_hat.abs() ** 2, dim=0)
+    A = torch.min(diag).values
+    B = torch.max(diag).values
+    kappa = B/A
+    if D == 1:
+        alias = 0
     else:
-        w_hat = torch.sum(torch.fft.fft(w, dim=1).abs() ** 2, dim=0)
-    B = torch.max(w_hat).item()
-    A = torch.min(w_hat).item()
-    return A, B
+        hop = w.shape[1]//D
+        alias = torch.zeros(w_hat.shape)
+        for j in range(1,D):
+            alias += torch.abs(w_hat * torch.conj(torch.roll(w_hat,j*hop,1)))
+        alias = torch.sum(alias, dim=0)
+    return kappa, alias # minimize the sum of them
 
 def fir_tightener3000(w, supp, eps=1.01):
     """
@@ -172,9 +154,8 @@ def fir_tightener3000(w, supp, eps=1.01):
         kappa = B / A
         #error = np.linalg.norm(w - w_tight)
         #if print_kappa:
-        #print("kappa:", "%.4f" % kappa)
+            #print("kappa:", "%.4f" % kappa, ", error:", "%.4f" % error)
     return w_tight
-
 
 def fir_tightener4000(w, supp, eps=1.01):
     """
@@ -184,7 +165,7 @@ def fir_tightener4000(w, supp, eps=1.01):
     :param eps: desired precision for kappa = B/A
     :return: approximately tight filterbank, where every filter is additionally a tight filterbank
     """
-    for i in tqdm.tqdm(range(w.shape[0]), desc="Tightening filters with fir-tightener4000"):
+    for i in range(w.shape[0]):
         filter = w[i,:].reshape(1,-1)
         w[i,:] = fir_tightener3000(filter, supp, eps)
     return w
