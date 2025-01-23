a709740a33a73785026455944f27fecebcfed29a
Felix Perfler <me@felixperfler.com>
2025-01-23 16:06:35 +0100

    removed unused code

--- hybra/utils.py
+++ hybra/utils.py
@@ -13,15 +13,6 @@ def calculate_condition_number(w) -> torch.Tensor:
     kappa: torch.Tensor = B/A
     return kappa
 
-def audfilters(n_filters, filter_length, hop_length, frequency_scale, sr):
-    """""
-    Constructs a set of filters *g* that are equidistantly spaced on a perceptual frequency scale (see |freqtoaud|) between 0 and the Nyquist frequency.
-    The filter bandwidths are proportional to the  critical bandwidth of the auditory filters |audfiltbw|.
-    The filters are intended to work with signals with a sampling rate of *fs*.
-    The signal length *Ls* is mandatory, since we need to avoid too narrow frequency windows.
-    """""
-    g = np.zeros((n_filters, filter_length), dtype=np.complex64)
-    return g
 
 def smooth_fir(frequency_responses, support):
     """""
