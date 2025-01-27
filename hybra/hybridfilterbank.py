319920b51fdaa78301771cd7ce5f8fad3a096470
Felix Perfler <me@felixperfler.com>
2025-01-27 14:54:56 +0100

    that was a typo

--- hybra/hybridfilterbank.py
+++ hybra/hybridfilterbank.py
@@ -21,12 +21,12 @@ class HybrA(nn.Module):
         self.output_real_forward = None
         self.output_imag_forward = None
 
-        k = torch.tensor(self.n_filters / (self.kernel_size * self.n_filters))
-        encoder_weight = (-torch.sqrt(k) - torch.sqrt(k)) * torch.rand([self.n_filters, 1, self.kernel_size]) + torch.sqrt(k)
+        k = torch.tensor(self.n_filters / (L * self.n_filters))
+        encoder_weight = (-torch.sqrt(k) - torch.sqrt(k)) * torch.rand([self.n_filters, 1, L]) + torch.sqrt(k)
 
         if start_tight:
             encoder_weight = torch.tensor(fir_tightener3000(
-                encoder_weight.squeeze(1).numpy(), self.kernel_size, eps=1.01
+                encoder_weight.squeeze(1).numpy(), L, eps=1.01
             ),  dtype=torch.float32).unsqueeze(1)
         
         self.encoder_weight_real = nn.Parameter(encoder_weight, requires_grad=True)
@@ -66,13 +66,13 @@ class HybrA(nn.Module):
         output_real = F.conv1d(
             F.pad(x.unsqueeze(1), (self.fir_filter_len//2, self.fir_filter_len//2), mode='circular'),
             kernel_real,
-            stride=self.fir_fitler_stride,
+            stride=self.fir_filter_stride,
         )
         
         output_imag = F.conv1d(
             F.pad(x.unsqueeze(1), (self.fir_filter_len//2,self.fir_filter_len//2), mode='circular'),
             kernel_imag,
-            stride=self.fir_fitler_stride,
+            stride=self.fir_filter_stride,
         )
 
         out = output_real + 1j* output_imag
@@ -86,11 +86,11 @@ class HybrA(nn.Module):
         out = F.conv1d(
                     F.pad(x.unsqueeze(1),(self.fir_filter_len//2, self.fir_filter_len//2), mode='circular'),
                     self.hybra_filters_real.to(x.device),
-                    stride=self.fir_fitler_stride,
+                    stride=self.fir_filter_stride,
                 ) + 1j * F.conv1d(
                     F.pad(x.unsqueeze(1),(self.fir_filter_len//2, self.fir_filter_len//2), mode='circular'),
                     self.hybra_filters_imag.to(x.device),
-                    stride=self.fir_fitler_stride,
+                    stride=self.fir_filter_stride,
                 )
                 
         return out
@@ -110,13 +110,13 @@ class HybrA(nn.Module):
             F.conv_transpose1d(
                 x_real,
                 self.hybra_filters_real,
-                stride=self.fir_fitler_stride,
+                stride=self.fir_filter_stride,
                 padding=self.fir_filter_len//2,
             )
             + F.conv_transpose1d(
                 x_imag,
                 self.hybra_filters_imag,
-                stride=self.fir_fitler_stride,
+                stride=self.fir_filter_stride,
                 padding=self.fir_filter_len//2,
             )
         )
