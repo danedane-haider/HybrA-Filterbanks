65202617d5363c20dbc4412bc797e9fd219f19e6
Daniel Haider <leinad.rediah@gmail.com>
2025-02-03 12:45:57 +0100

    mini

--- test.py
+++ test.py
@@ -6,11 +6,3 @@ a = AudletFIR(decoder=True)
 
 a.plot_response()
 a.plot_decoder_response()
-
-# plt.plot(np.real(g[0,:]))
-# plt.plot(np.imag(g[0,:]))
-# plt.plot(np.real(g[1,:]))
-# plt.plot(np.imag(g[1,:]))
-# plt.plot(np.real(g[2,:]))
-# plt.plot(np.imag(g[2,:]))
-# plt.show()
