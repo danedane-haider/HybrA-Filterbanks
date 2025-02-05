4529ac4334a1992cb72d449cbc37af610d20f3d4
Felix Perfler <me@felixperfler.com>
2025-02-05 11:23:28 +0100

    lala

--- hybra/__init__.py
+++ hybra/__init__.py
@@ -1,7 +1,3 @@
 from .hybridfilterbank import HybrA
 from .fir_audlet_filterbank import AudletFIR
-<<<<<<< HEAD
 from .fir_mel_filterbank import AudSpec
-=======
-from .fir_mel_filterbank import MelFIR
->>>>>>> 7745df7ebe5bf8a8cf4f13c888eea09b3592052f
--- hybra/fir_mel_filterbank.py
+++ hybra/fir_mel_filterbank.py
@@ -1,22 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-<<<<<<< HEAD
 from hybra.utils import audfilters_fir
 from hybra.utils import plot_response as plot_response_
 
 class AudSpec(nn.Module):
     def __init__(self, filterbank_config={'filter_len':256,
                                           'num_channels':42,
-=======
-from hybra.utils import audfilters_fir, fir_tightener3000
-from hybra.utils import plot_response as plot_response_
-from hybra._fit_neurodual import fit
-
-class MelFIR(nn.Module):
-    def __init__(self, filterbank_config={'filter_len':256,
-                                          'num_channels':64,
->>>>>>> 7745df7ebe5bf8a8cf4f13c888eea09b3592052f
                                           'fs':16000,
                                           'Ls':16000,
                                           'bwmul':1},
@@ -31,10 +21,7 @@ class MelFIR(nn.Module):
         self.fs = filterbank_config['fs']
         self.fc = fc
         self.fc_crit = fc_crit
-<<<<<<< HEAD
         self.num_channels = filterbank_config['num_channels']
-=======
->>>>>>> 7745df7ebe5bf8a8cf4f13c888eea09b3592052f
 
         kernels_real = torch.tensor(filters.real, dtype=torch.float32)
         kernels_imag = torch.tensor(filters.imag, dtype=torch.float32)
@@ -48,36 +35,20 @@ class MelFIR(nn.Module):
 
     def forward(self, x):
         x = F.conv1d(
-<<<<<<< HEAD
             F.pad(x, (self.filter_len//2, self.filter_len//2), mode='circular'),
             self.kernels_real.unsqueeze(1),
             stride=self.stride,
         )**2 + F.conv1d(
             F.pad(x, (self.filter_len//2,self.filter_len//2), mode='circular'),
             self.kernels_imag.unsqueeze(1),
-=======
-            F.pad(x.unsqueeze(1), (self.filter_len//2, self.filter_len//2), mode='circular'),
-            self.kernel_real,
-            stride=self.stride,
-        )**2 + F.conv1d(
-            F.pad(x.unsqueeze(1), (self.filter_len//2,self.filter_len//2), mode='circular'),
-            self.kernel_imag,
->>>>>>> 7745df7ebe5bf8a8cf4f13c888eea09b3592052f
             stride=self.stride,
         )**2
         
         output = F.conv1d(
             x,
-<<<<<<< HEAD
             self.kernels_real[0,:].repeat(self.num_channels,1).to(x.device).unsqueeze(1),
             groups=self.num_channels
         )
-=======
-            self.kernels_real[0,:].to(x.device),
-            groups=self.num_channels,
-            padding="same",
-        ).unsqueeze(1)
->>>>>>> 7745df7ebe5bf8a8cf4f13c888eea09b3592052f
 
         return output
 
