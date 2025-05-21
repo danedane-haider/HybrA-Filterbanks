79fc9315db6e521ecfa43d39cfa6cc2f39659e0e
Felix Perfler <me@felixperfler.com>
2025-05-21 13:56:09 +0200

    more

--- docs/api.rst
+++ docs/api.rst
@@ -1,6 +1,18 @@
-
-HybrA-Filterbanks
+hybra
 =================
 
-.. automodule:: hybra
+.. autoclass:: hybra.HybrA
 	:members:
+	
+.. autoclass:: hybra.ISAC
+	:members:
+	
+.. autoclass:: hybra.ISACMelSpectrogram
+	:members:
+
+hybra.utils
+===========
+
+.. automodule:: hybra.utils
+	:members:
+
--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,13 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
