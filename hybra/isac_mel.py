beb3fb21a8d44f3b1ffb28e3826fbf445eda4bd8
Daniel Haider <leinad.rediah@gmail.com>
2025-07-28 00:31:40 +0200

    mel_spectrogram bug fixed

--- hybra/isac_mel.py
+++ hybra/isac_mel.py
@@ -4,11 +4,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from hybra.utils import audfilters
+from hybra.utils import audfilters, circ_conv
 from hybra.utils import plot_response as plot_response_
 from hybra.utils import ISACgram as ISACgram_
 
-class ISACMelSpectrogram(nn.Module):
+class MelSpec(nn.Module):
+    """Constructor for an ISAC Mel spectrogram filterbank.
+    Args:
+        kernel_size (int) - size of the kernels of the auditory filterbank
+        num_channels (int) - number of channels
+        fc_max (float) - maximum frequency on the auditory scale. if 'None', it is set to fs//2.
+        stride (int) - stride of the auditory filterbank. if 'None', stride is set to yield 25% overlap
+        fs (int) - sampling frequency
+        L (int) - signal length
+        supp_mult (float) - support multiplier.
+        scale (str) - auditory scale ('mel', 'erb', 'bark', 'log10', 'elelog'). elelog is a scale adapted to the hearing of elephants
+        is_encoder_learnable (bool) - whether the encoder kernels are learnable
+        is_averaging_kernel_learnable (bool) - whether the averaging kernel is learnable
+        is_log (bool) - whether to apply log10 to the output
+        verbose (bool) - whether to print information about the filterbank
+    """
     def __init__(self,
                  kernel_size:Union[int,None]=None,
                  num_channels:int=40,
@@ -16,23 +31,30 @@ class ISACMelSpectrogram(nn.Module):
                  stride:Union[int,None]=None,
                  fs:int=16000, 
                  L:int=16000,
-                 bw_multiplier:float=1,
-                 scale:str='erb',
+                 supp_mult:float=1,
+                 scale:str='mel',
                  is_encoder_learnable=False,
                  is_averaging_kernel_learnable=False,
-                 is_log=False):
+                 is_log=False,
+                 verbose:bool=True):
         super().__init__()
 
-        [kernels, d, fc, fc_min, fc_max, kernel_min, kernel_size, Ls] = audfilters(
-            kernel_size=kernel_size,num_channels=num_channels, fc_max=fc_max, fs=fs,L=L,bw_multiplier=bw_multiplier,scale=scale
+        [aud_kernels, d, fc, fc_min, fc_max, kernel_min, kernel_size, Ls] = audfilters(
+            kernel_size=kernel_size,num_channels=num_channels, fc_max=fc_max, fs=fs,L=L, supp_mult=supp_mult,scale=scale
         )
 
         if stride is not None:
             d = stride
             Ls = int(torch.ceil(torch.tensor(L / d)) * d)
-        print(f"The output length is set to {Ls}.")
 
-        self.kernels = kernels
+        if verbose:
+            print(f"Max. kernel size: {kernel_size}")
+            print(f"Min. kernel size: {kernel_min}")
+            print(f"Number of channels: {num_channels}")
+            print(f"Stride for min. 25% overlap: {d}")
+            print(f"Signal length: {Ls}")
+
+        self.num_channels = num_channels
         self.stride = d
         self.kernel_size = kernel_size
         self.kernel_min = kernel_min
@@ -40,40 +62,26 @@ class ISACMelSpectrogram(nn.Module):
         self.fc = fc
         self.fc_min = fc_min
         self.fc_max = fc_max
-        self.num_channels = num_channels
         self.Ls = Ls
 
-        self.time_avg = self.kernel_size // self.stride
+        self.time_avg = int(self.kernel_size // self.stride)
         self.time_avg_stride = self.time_avg // 2
-
-        kernels_real = kernels.real.to(torch.float32)
-        kernels_imag = kernels.imag.to(torch.float32)
 
         self.is_log = is_log
 
         if is_encoder_learnable:
-            self.register_parameter('kernels_real', nn.Parameter(kernels_real, requires_grad=True))
-            self.register_parameter('kernels_imag', nn.Parameter(kernels_imag, requires_grad=True))
+            self.register_parameter('kernels', nn.Parameter(aud_kernels, requires_grad=True))
         else:
-            self.register_buffer('kernels_real', kernels_real)
-            self.register_buffer('kernels_imag', kernels_imag)
+            self.register_buffer('kernels', aud_kernels)
 
         if is_averaging_kernel_learnable:
-            self.register_parameter('averaging_kernel', nn.Parameter(torch.ones([self.num_channels,1,self.time_avg]), requires_grad=True))
+            self.register_parameter('averaging_kernel', nn.Parameter(torch.ones([self.num_channels,1,self.time_avg], dtype=aud_kernels.dtype), requires_grad=True))
         else:
-            self.register_buffer('averaging_kernel', torch.ones([self.num_channels,1,self.time_avg]))
+            self.register_buffer('averaging_kernel', torch.ones([self.num_channels,1,self.time_avg], dtype=aud_kernels.dtype))
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
+        x = circ_conv(x.unsqueeze(1), self.kernels, self.stride)
         x = F.conv1d(
-            F.pad(x, (self.kernel_size//2, self.kernel_size//2), mode='circular'),
-            self.kernels_real.unsqueeze(1),
-            stride=self.stride,
-        )**2 + F.conv1d(
-            F.pad(x, (self.kernel_size//2,self.kernel_size//2), mode='circular'),
-            self.kernels_imag.unsqueeze(1),
-            stride=self.stride,
-        )**2
-        output = F.conv1d(
             x,
             self.averaging_kernel.to(x.device),
             groups=self.num_channels,
@@ -81,9 +89,9 @@ class ISACMelSpectrogram(nn.Module):
         )
 
         if self.is_log:
-            output = torch.log10(output)
+            x = torch.log10(x)
 
-        return output
+        return x
 
     def ISACgram(self, x):
         with torch.no_grad():
@@ -91,4 +99,4 @@ class ISACMelSpectrogram(nn.Module):
         ISACgram_(coefficients, self.fc, self.Ls, self.fs)
 
     def plot_response(self):
-        plot_response_(g=(self.kernels_real + 1j*self.kernels_imag).detach().numpy(), fs=self.fs, scale=True, fc_min=self.fc_min, fc_max=self.fc_max, kernel_min=self.kernel_min)
+        plot_response_(g=(self.kernels).detach().numpy(), fs=self.fs, scale=True, fc_min=self.fc_min, fc_max=self.fc_max, kernel_min=self.kernel_min)
